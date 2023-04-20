e92fa572cc2f94cfacce4d7bbb06039333b5ba7e
xm <1749207188@qq.com>
2023-04-20 21:09:51 +0800

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html', './src/**/*.{html,js,ts,jsx,tsx,vue}'],
+  content: [
+    "./public/index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx,vue}",
+    "!./src/wxcomponents/**/*",
+    "!./src/uni_modules/**/*",
+  ],
   corePlugins: {
     // 不需要 preflight，因为这主要是给 h5 的，如果你要同时开发小程序和 h5 端，你应该使用环境变量来控制它
-    preflight: false
-  }
-}
+    preflight: false,
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin as uvwt } from 'weapp-tailwindcss-webpack-plugin/vite';
 
-const vitePlugins = [uni(),uvwt()]
+const vitePlugins = [uni(),uvwt({
+  appType: 'uni-app-vite'
+})]
 
 const postcssPlugins = [require('autoprefixer')(), require('tailwindcss')()];
 
