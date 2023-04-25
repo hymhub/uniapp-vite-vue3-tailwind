89461c4115a8017be08bab7e335e1ac49fc7b934
xm <1749207188@qq.com>
2023-04-25 20:24:39 +0800

    fix: 工程化启动

---
+++ README.md
@@ -0,0 +1,15 @@
+# uniapp-vite-vue3-tailwind
+
+在 uniapp 中使用 tailwind 的一个基础项目架子，主要用于微信小程序
+
+## Usage
+
+```shell
+npm run start
+```
+
+## FAQ
+
+> 目前微信开发者工具会默认开启 `代码自动热重载 (compileHotReLoad)` 功能，这个功能在原生开发中表现良好，但在 `uni-app` 和 `taro` 等等的框架中，可能存在一定的问题，建议关闭 `代码自动热重载` 功能。
+
+更多有关 tailwind 插件问题可以去 [weapp-tailwindcss-webpack-plugin](https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin) 看
--- unistart.js
+++ unistart.js
@@ -1,5 +1,8 @@
-const inquirer = require('inquirer');
-var shell = require('shelljs');
+const inquirer = require('inquirer')
+const shell = require('shelljs')
+const os = require('os')
+const path = require('path')
+const fs = require('fs')
 
 inquirer
   .prompt([
@@ -36,23 +39,63 @@ inquirer
     }
   ])
   .then(({ script }) => {
-    Promise.resolve().then(() => {
-      if (shell.exec(`npm run ${script}`).code !== 0) {
-        shell.echo(`Error: npm run ${script}`);
-        shell.exit(1);
-      }
+    if (shell.exec('chcp 65001').code !== 0) {
+      shell.echo('chcp 65001 Error')
+      shell.exit(1)
+    }
+    shell.exec(`npm run ${script}`, function (code, stdout, stderr) {
+      console.log('Exit code:', code);
+      console.log('Program output:', stdout);
+      console.log('Program stderr:', stderr);
     })
     if (script.endsWith('mp-weixin')) {
-      if (script.startsWith('dev')) {
-        if (shell.exec('weapp open -p dist/dev/mp-weixin').code !== 0) {
-          shell.echo('Open WeiXin IDE Error: weapp open -p dist/dev/mp-weixin');
-          shell.exit(1);
+      let dontOpen = false
+      const SupportedPlatformsMap = {
+        Windows_NT: 'Windows_NT',
+        Darwin: 'Darwin'
+      };
+      const defaultPathMap = {
+        [SupportedPlatformsMap.Windows_NT]: 'C:\\Program Files (x86)\\Tencent\\微信web开发者工具\\cli.bat',
+        [SupportedPlatformsMap.Darwin]: '/Applications/wechatwebdevtools.app/Contents/MacOS/cli'
+      };
+      const defaultCustomConfigFilePath = path.join(path.join(os.homedir(), '.weapp-ide-cli'), 'config.json');
+      const defaultPath = defaultPathMap[os.type()];
+      let configInfo1
+      let configInfo2
+      try {
+        configInfo1 = fs.statSync(defaultCustomConfigFilePath)
+      } catch (error) { }
+      try {
+        configInfo2 = fs.statSync(defaultPath)
+      } catch (error) { }
+      if (!configInfo1?.isFile() && !configInfo2?.isFile()) {
+        dontOpen = true
+      }
+      if (dontOpen) {
+        shell.echo('\x1b[31m%s\x1b[0m', '首次运行请先执行 npm run initide 设置小程序开发者工具路径后再重新 npm run start')
+        shell.exit(1)
+      }
+      const config = fs.readFileSync(path.join(__dirname, './src/manifest.json'), 'utf8')
+      if (config.match(/"mp-weixin"\s*:[\w\W]+?"appid"\s*:\s*"(.*?)"/)?.[1].length === 0) {
+        console.log('\x1b[34m%s\x1b[0m', '没有配置小程序 appid, 建议在 src/manifest.json 中进行配置')
+      }
+      const openIde = () => {
+        let ins
+        if (script.startsWith('dev')) {
+          ins = shell.exec('weapp open -p dist/dev/mp-weixin', { silent: true })
+        } else if (script.startsWith('build')) {
+          ins = shell.exec('weapp open -p dist/build/mp-weixin', { silent: true })
         }
-      } else if(script.startsWith('build')) {
-        if (shell.exec('weapp open -p dist/build/mp-weixin').code !== 0) {
-          shell.echo('Open WeiXin IDE Error: weapp open -p dist/build/mp-weixin');
-          shell.exit(1);
+        if (ins.code !== 0) {
+          shell.echo('Open WeiXin IDE Error: weapp open -p dist/dev/mp-weixin')
+          shell.exit(1)
+        }
+        if (ins.stdout.includes('Error')) {
+          setTimeout(() => {
+            openIde()
+          }, 1000)
         }
       }
+      openIde()
     }
   })
